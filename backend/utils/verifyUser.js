5aa9146513491475af948feba0c675d981724537
Josephumoren001 <josephumoren017@gmail.com>
2024-12-04 04:55:49 +0100

    Update verifyUser.js

--- backend/utils/verifyUser.js
+++ backend/utils/verifyUser.js
@@ -1,28 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { errorHandler } from './error.js';
 
-// export const verifyToken = async (req, res, next) => {
-//   try {
-//     const token = req.cookies.access_token || req.headers['authorization']?.split(' ')[1];
-//     console.log('Received Token:', token);
-//     if (!token) {
-//       return next(errorHandler(401, "Unauthorized - No token provided"));
-//     }
-
-//     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-//       if (err) {
-//         console.log('Token Verification Failed:', err.message);
-//         return next(errorHandler(401, "Unauthorized - Invalid token"));
-//       }
-//       req.user = decoded;
-//       next();
-//     });
-//   } catch (error) {
-//     console.error("Token verification error:", error);
-//     next(errorHandler(500, "An error occurred during token verification"));
-//   }
-// };
-
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token || req.headers['authorization']?.split(' ')[1];
   console.log('Received Token:', token); // Log the token
